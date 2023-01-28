2092d1948ecccd8aac0aec66911d33d31cc9ce39
Loskon <andreyrochev23@gmail.com>
2023-01-28 14:38:44 +0300

    Initial commit

---
+++ app/src/main/java/com/loskon/sportapi/App.kt
@@ -0,0 +1,13 @@
+package com.loskon.sportapi
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
